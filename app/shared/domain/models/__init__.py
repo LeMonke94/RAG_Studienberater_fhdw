c7f0fed6b330616dfa150b93a844ab6e1e96371d
LeMonke <shresthaalex365@gmail.com>
2026-03-29 01:38:29 +0100

    feat: initialising models as module

---
+++ app/shared/domain/models/__init__.py
@@ -0,0 +1,15 @@
+# Imports
+from .answer import Answer
+from .chunk import Chunk
+from .document import Document
+from .query import Query
+from .retrieval_result import RetrievalResult
+
+# Export für *
+__all__ = {
+    'Anwer',
+    'Chunk',
+    'Document',
+    'Query',
+    'RetrievalResult'
+}
